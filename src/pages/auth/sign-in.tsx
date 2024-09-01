bab3ad759182e058d403a917b32dd508e2bd3137
Victor Raony <victor_raony12@hotmail.com>
2024-09-01 13:49:24 -0300

    Registration screen

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -3,6 +3,7 @@ import { Label } from '@/components/ui/label'
 import { Input } from '@/components/ui/input'
 
 import {useForm} from 'react-hook-form'
+import {Link} from 'react-router-dom'
 
 import {z} from 'zod'
 import {toast} from 'sonner'
@@ -37,6 +38,11 @@ export function SignIn() {
     <>
       <Helmet title='Login'/>
       <div className='p-8'>
+        <Button variant='ghost' asChild className='absolute top-8 right-8' >
+            <Link to='/sign-up'>
+              Novo estabelecimento
+            </Link>
+        </Button>
         <div className='w-[350px] flex flex-col justify-center gap-6'>
           <div className='flex flex-col gap-2 text-center'>
             <h1 className='text-2xl font-semibold tracking-tight'>
