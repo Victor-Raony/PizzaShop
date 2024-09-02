d7d5f7d5dab867f86cad269845450bf41b04bdc0
Victor Raony <victor_raony12@hotmail.com>
2024-09-02 00:25:12 -0300

    Implementation of the order listing screen

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,6 +3,9 @@ import { Separator } from "./ui/separator";
 
 import { NavLink } from "./nav-link";
 
+import { ThemeToggle } from "./theme/theme-toggle";
+import { AccountMenu } from "./account-menu";
+
 export function Header() {
   return(
     <div className="border-b">
@@ -22,6 +25,10 @@ export function Header() {
               Pedidos
           </NavLink>
         </nav>
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+          <AccountMenu />
+        </div>
       </div>
     </div>
   )
